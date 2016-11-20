e06e59a76ec0ebff7af69dfc32119d9a8eaf5326
weasterner <yingjin.us@gmail.com>
2016-11-19 20:39:56 -0500

    File I/O error handling

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var fs = require('fs');
-fs.readFile('package.json', (err,buffer) => {
-  console.log(buffer.toString());
+fs.readdir('.',(err,files) => {
+  if(err){
+    console.log('Error finding files:'+err);
+  }
+  else{
+    files.forEach((fileName,fileIndex) => {
+      fs.readFile(fileName,(err, buffer) => {
+        if(err){
+          console.log('Error reading file:'+err);
+        }
+        else{
+          console.log(buffer.toString());
+        }
+      })
+    });
+  }
 })

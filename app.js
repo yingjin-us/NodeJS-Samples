8282dbe82204b0f616fe715f4a263c6a83f5b145
weasterner <yingjin.us@gmail.com>
2016-11-19 20:58:37 -0500

    Stream pipe with fs

--- app.js
+++ app.js
@@ -1,18 +1,2 @@
 var fs = require('fs');
-fs.readdir('.',(err,files) => {
-  if(err){
-    console.log('Error finding files:'+err);
-  }
-  else{
-    files.forEach((fileName,fileIndex) => {
-      fs.readFile(fileName,(err, buffer) => {
-        if(err){
-          console.log('Error reading file:'+err);
-        }
-        else{
-          console.log(buffer.toString());
-        }
-      })
-    });
-  }
-})
+fs.createReadStream('server.js').pipe(fs.createWriteStream('out.js'));

d746501d04908310a700ef32b8693e93853d818e
weasterner <yingjin.us@gmail.com>
2016-11-19 20:21:59 -0500

    Hello world in HTTP

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+var http=require('http');
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    res.end("Hello World\n");
+});
+
+server.listen(7000);

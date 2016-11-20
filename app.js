6334c63d2ec9fd6e56c6178bcd87cb801f6b5c06
weasterner <yingjin.us@gmail.com>
2016-11-19 20:24:57 -0500

    Sync File I/O with fs

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-var http=require('http');
-
-var server = http.createServer(function(req, res){
-    res.writeHead(200,{"Content-Type":"text/plain"});
-    res.end("Hello World\n");
-});
-
-server.listen(7000);
+var fs = require('fs');
+var contents = fs.readFileSync('package.json').toString();
+console.log(contents);
